414fe17ecc11720330d9ee7e3299b43a905b7e0b
abu-kausar <abukausar33@gmail.com>
2024-09-15 01:04:45 +0600

    toggle password added

--- src/components/applyStates/InputField.tsx
+++ src/components/applyStates/InputField.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FieldError, Path, UseFormRegister, FieldValues } from 'react-hook-form';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 interface InputFieldProps<T extends FieldValues> { // Add the constraint here
     label: string;
@@ -11,24 +12,41 @@ interface InputFieldProps<T extends FieldValues> { // Add the constraint here
     required?: string;
 }
 
-const InputField = <T extends FieldValues>({ 
-    label, 
-    name, 
-    placeholder, 
-    type = "text", 
-    register, 
-    error, 
-    required 
+const InputField = <T extends FieldValues>({
+    label,
+    name,
+    placeholder,
+    type = "text",
+    register,
+    error,
+    required
 }: InputFieldProps<T>) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const inputType = type === 'password' && showPassword ? 'text' : type;
+
     return (
         <div className='w-full flex flex-col'>
             <h3 className='text-sm font-medium text-[#344054] mb-[6px]'>{label}</h3>
             <input
-                type={type}
+                type={inputType}
                 {...register(name, { required })}
                 placeholder={placeholder}
-                className='p-[10px_14px] border border-[#D0D5DD] rounded-[52px] outline-none focus:border-[#BB7AFE]'
+                className='relative p-[10px_14px] border border-[#D0D5DD] rounded-[52px] outline-none focus:border-[#BB7AFE]'
             />
+
+            {
+                type==='password' &&
+                <span 
+                    onClick={() => setShowPassword(!showPassword)} 
+                    className='absolute mt-10 right-12 cursor-pointer'
+                >
+                    {
+                        showPassword ? <FaEye/> :<FaEyeSlash/>
+                    }
+                </span>
+            }
+
             {error && <span className="text-red-500">{error.message}</span>}
         </div>
     );
